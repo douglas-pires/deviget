de2eb49f303d66cbd04e635e51fdff4c232aa2d5
Douglas Pires <douglas.pires@live.com>
2019-12-16 00:44:12 -0300

    remove unecessary imports

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import { VDataTable, VImg } from "vuetify";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-  VDataTable,
-  VImg
-});
+export default new Vuetify({});
