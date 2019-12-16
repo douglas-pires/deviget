ef025858adfa0cd2b99e3b510f3ce9cdb997ff9c
Douglas Pires <douglas.pires@live.com>
2019-12-15 23:24:25 -0300

    complementary store mutations

--- src/store/modules/posts/mutations.js
+++ src/store/modules/posts/mutations.js
@@ -2,13 +2,22 @@ import * as types from "./types";
 
 const handlePosts = rawData => {
   return rawData.children.map((post, index) => {
-    const { author, title, thumbnail, created_utc, num_comments } = post.data;
+    const {
+      author,
+      title,
+      thumbnail,
+      created_utc,
+      num_comments,
+      id
+    } = post.data;
     return {
+      id,
       author,
       title,
       thumbnail,
       entryDate: new Date(created_utc * 1000).toLocaleDateString(),
       commentsLength: num_comments,
+      unreadStatus: false,
       rawData: rawData.children[index].data
     };
   });
@@ -17,5 +26,27 @@ const handlePosts = rawData => {
 export default {
   [types.MUT_SET_TOP_POSTS](state, rawData) {
     state[types.STATE_TOP_POSTS] = handlePosts(rawData);
+  },
+  [types.MUT_SET_SELECTED_POST](state, post) {
+    state[types.STATE_SELECTED_POST] = post;
+  },
+  [types.MUT_DELETE_POST](state, post) {
+    const posts = state[types.STATE_TOP_POSTS];
+
+    state[types.STATE_TOP_POSTS] = posts.filter(item => item.id !== post.id);
+  },
+  [types.MUT_SET_READ_STATUS](state, post) {
+    const posts = state[types.STATE_TOP_POSTS];
+
+    const idx = posts.findIndex(item => item.id === post.id);
+
+    if (!posts[idx]) return;
+
+    posts[idx].unreadStatus = true;
+
+    state[types.STATE_TOP_POSTS] = [...posts];
+  },
+  [types.MUT_DELETE_ALL](state) {
+    state[types.STATE_TOP_POSTS] = [];
   }
 };
--- src/store/modules/posts/state.js
+++ src/store/modules/posts/state.js
@@ -1,5 +1,7 @@
 import * as types from "./types";
 
 export default () => ({
-  [types.STATE_TOP_POSTS]: []
+  [types.STATE_TOP_POSTS]: [],
+  [types.STATE_SELECTED_POST]: {},
+  [types.STATE_SELECTED_POST]: null
 });
