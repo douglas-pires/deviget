75654909c12dc20c4940a65dac28eb29093fc5ef
Douglas Pires <douglas.pires@live.com>
2019-12-16 00:42:16 -0300

    add plugins

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import "./style/main.scss";
+import "./plugins";
 
 Vue.config.productionTip = false;
 
